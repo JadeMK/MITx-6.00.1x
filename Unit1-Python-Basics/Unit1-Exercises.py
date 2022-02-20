e79fdeeee6c18a2299ae1ea4618d9ba421dbddb4
Lily Kim <97623334+KimLillian@users.noreply.github.com>
2022-02-21 01:56:41 +0900

    Add files via upload

---
+++ Unit1-Python-Basics/Unit1-Exercises.py
@@ -0,0 +1,41 @@
+# Unit 1: Python baxics - 2. Core Elements of Progrmas - Exercise
+
+
+""" Write a piece of Python code that prints out the string hello world."""
+
+print('hello world')
+
+
+""" Prints out the string 'hello world' if the value of an integer variable,
+happy, is strictly greater than 2."""
+
+if happy > 2:
+    print('hello world')
+    
+
+""" Convert the following into code that uses a while loop.
+prints 2
+prints 4
+prints 6
+prints 8
+prints 10
+prints Goodbye! """
+
+i = 2
+while i < 11:
+    print(i)
+    i += 2
+print('Goodbye!')
+
+
+""" Convert the following code into code that uses a for loop.
+prints 2
+prints 4
+prints 6
+prints 8
+prints 10
+prints Goodbye! """
+
+for i in range(2, 11, 2):
+    print(i)
+print('Goodbye!')
