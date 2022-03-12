5106390ff0cae70ff02529baf2c0be2c8ca6745d
Lily Kim <97623334+KimLillian@users.noreply.github.com>
2022-03-13 00:08:08 +0900

    Add files via upload

---
+++ Unit-4-Good-Programming-Practices/Testing-and-debugging/Integer-division.py
@@ -0,0 +1,38 @@
+"""
+Your task is to modify the code for integerDivision so that this error does not occur.
+
+def integerDivision(x, a):
+    '''
+    x: a non-negative integer argument
+    a: a positive integer argument
+
+    returns: integer, the integer division of x divided by a.
+    '''
+    while x >= a:
+        count += 1
+        x = x - a
+    return count
+
+print(integerDivision(5, 3))
+
+File "temp.py", line 9, in integerDivision
+    count += 1
+UnboundLocalError: local variable 'count' referenced before assignment
+"""
+
+
+def integerDivision(x, a):
+    """
+    x: a non-negative integer argument
+    a: a positive integer argument
+
+    returns: integer, the integer division of x divided by a.
+    """
+    count = 0
+    while x >= a:
+        count += 1
+        x = x - a
+    return count
+
+
+# print(integerDivision(5, 3))
